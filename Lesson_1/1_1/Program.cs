042a2f590cbea32c9dc41d3884f6151301dbd86c
Eeveona <eeveona@gmail.com>
2024-01-07 01:42:14 +0300

    Classwork task 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Вывод текста в строку, (обновить информацию позже)
+Console.Write("Enter a number: ");
+string str1 = Console.ReadLine()!;
+
+Console.WriteLine(str1 + str1);
+
+int num1 = int.Parse(str1);
+Console.WriteLine(num1 + num1);
+
+Console.WriteLine(num1 * num1);
